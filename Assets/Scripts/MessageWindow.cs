d415c5b071fc762aa38125c25030575ba7541776
HarashimaYoshikazu <harashima318@gmail.com>
2022-10-14 11:53:07 +0900

    [update]課題2:入力でテキスト送りができるようになった。

--- Assets/Scripts/MessageWindow.cs
+++ Assets/Scripts/MessageWindow.cs
@@ -13,11 +13,35 @@ public class MessageWindow : MonoBehaviour
     [SerializeField]
     Text _letterText = null;
 
+    [Header("デバッグ")]
+    [SerializeField]
+    List<string> _letters = new List<string>();
+    int _letterIndex = 0;
+
     private void Awake()
     {
-        if (_windowPanel||!_nameText || !_letterText)
+        if (!_windowPanel||!_nameText || !_letterText)
         {
             Debug.LogError($"Textコンポーネントがアタッチされていません：WindowPanel{_windowPanel}NameText{_nameText},LetterText{_letterText}");
         }
+    }
+
+    private void Update()
+    {
+        DebugText(Input.GetButtonDown("Fire1"), Input.GetKeyDown(KeyCode.Space));
+    }
+
+    private void DebugText(bool input)
+    {
+        if (input)
+        {
+            _letterText.text = _letters[_letterIndex % _letters.Count];
+            _letterIndex++;
+        }
+    }
+    private void DebugText(bool input,bool input2)
+    {
+        DebugText(input);
+        DebugText(input2);
     }
 }
